90c0ae884c5fe11283ae63afd3b8c52a13ef12ac
chengshoguang <chengshaoguang1291@gmail.com>
2014-08-10 11:33:31 +0800

    modify main UI

--- TDIRLP/mainwindow.cpp
+++ TDIRLP/mainwindow.cpp
@@ -7,8 +7,13 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
 
+    QString f("\n2x + 5y <= 9\n");
+    ui->label->setText(f);
+    f.append("3x + 4y <= 0");
+    ui->label->setText(f);
 
-    ui->customplot->setBackground(QBrush(QColor(50,50,50)));
+
+    ui->customplot->setBackground(QBrush(QColor(150,150,150)));
     ui->customplot->setInteractions(QCP::iRangeDrag | QCP::iRangeZoom | QCP::iSelectPlottables);
 
     ui->customplot->replot();
